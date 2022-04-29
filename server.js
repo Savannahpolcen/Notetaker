c24ad54dda833c69fbefa415053d2622ffe948bd
Savannahpolcen <Savannahpolcen@gmail.com>
2022-04-28 22:43:49 -0400

    store added / Heroku not working

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require ('express');
 const htmlRoutes = require('./routes/htmlRoutes')
 const APIroutes = require('./routes/APIroutes')
+
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 
